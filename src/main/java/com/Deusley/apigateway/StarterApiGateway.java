bfe8532359846fe36ae4de4784aab5bade781bc2
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-10-05 03:17:30 -0300

    ProjectInit

---
+++ src/main/java/com/Deusley/apigateway/StarterApiGateway.java
@@ -0,0 +1,13 @@
+package com.Deusley.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApiGateway {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApiGateway.class, args);
+	}
+
+}
