8235fb359b824ccf69b0ba85492d626b65e22a45
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-10-07 02:11:08 -0300

    update02

--- src/main/java/com/Deusley/apigateway/Starter.java
+++ src/main/java/com/Deusley/apigateway/Starter.java
@@ -2,12 +2,13 @@ package com.Deusley.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class StarterApiGateway {
+public class Starter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterApiGateway.class, args);
+		SpringApplication.run(Starter.class, args);
 	}
 
 }
