f132ade2d4cc05ca244b61fd36d33da471ea8147
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-10-09 23:35:14 -0300

    testRouters

--- src/main/java/com/Deusley/apigateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/Deusley/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,26 +1,29 @@
 package com.Deusley.apigateway.configuration;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-        Function<PredicateSpec, Buildable<Route>> function =
-                p -> p.path("/get")
-                        .filters(f -> f.addRequestHeader("TESTE", "SYSTEM"))
-                        .uri("http://httpbin.org:80");
         return builder.routes()
-                .route(function)
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("TESTE", "SYSTEM")
+                                .addRequestParameter("TEST2", "SYS2"))
+                        .uri("http://httpbin.org:80"))
+
+                .route(p -> p.path("/cambio-service/**")
+                        .uri("lb://cambio-service"))
+
+                .route(p -> p.path("/book-service/**")
+                        .uri("lb://book-service"))
+
                 .build();
     }
 
