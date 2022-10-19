5496ef4a10d315ed6b878b2b631123f4d2ae6013
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-10-19 03:16:47 -0300

    AjustRouters/implWebFluxSwgg

--- src/main/java/com/Deusley/apigateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/Deusley/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,4 +1,4 @@
-package com.Deusley.apigateway.configuration;
+//package com.Deusley.apigateway.configuration;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -6,25 +6,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
-public class ApiGatewayConfiguration {
+//@Configuration
+//public class ApiGatewayConfiguration {
 
-    @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-        return builder.routes()
-                .route(p -> p.path("/get")
-                        .filters(f -> f
-                                .addRequestHeader("TESTE", "SYSTEM")
-                                .addRequestParameter("TEST2", "SYS2"))
-                        .uri("http://httpbin.org:80"))
+    //@Bean
+    //public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        //return builder.routes()
+                //.route(p -> p.path("/get")
+                     //   .filters(f -> f
+                       //         .addRequestHeader("TESTE", "SYSTEM")
+                   //             .addRequestParameter("TEST2", "SYS2"))
+                 //       .uri("http://httpbin.org:80"))
 
-                .route(p -> p.path("/cambio-service/**")
-                        .uri("lb://cambio-service"))
+               // .route(p -> p.path("/cambio-service/**")
+                 //       .uri("lb://cambio-service"))
 
-                .route(p -> p.path("/book-service/**")
-                        .uri("lb://book-service"))
+               // .route(p -> p.path("/book-service/**")
+                 //       .uri("lb://book-service"))
 
-                .build();
-    }
+               // .build();
+ //   }
 
-}
+//}
